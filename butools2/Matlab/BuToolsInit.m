a2a718c41ff954d2cf2273eb1e8dd4615a7f17dc
alirezatko <taaghche.alibakhshi@gmail.com>
2023-03-13 16:24:27 +0330

    butools installation

---
+++ butools2/Matlab/BuToolsInit.m
@@ -0,0 +1,46 @@
+function BuToolsInit (verbose, checkInput, checkPrecision)
+
+    fname = mfilename('fullpath');
+    [pathstr, ~, ~] = fileparts(fname);
+
+    disp('<strong>Butools V2.0</strong>')
+    
+    packages = {'utils', 'mc', 'moments', 'reptrans', 'trace', 'ph', 'dph', 'map', 'dmap', 'mam', 'queues', 'fitting'};
+    
+    path(pathstr, path);
+    fprintf('Packages loaded: ');
+    for p=1:length(packages)
+        path(fullfile(pathstr,packages{p}), path);
+        fprintf(packages{p});
+        if p<length(packages)
+            fprintf(', ');
+        else
+            fprintf('\n');
+        end
+    end   
+    
+    global BuToolsVerbose;
+    global BuToolsCheckInput;
+    global BuToolsCheckPrecision;
+    
+    if exist('verbose','var')
+        BuToolsVerbose = verbose;
+    else
+        BuToolsVerbose = false;
+    end
+
+    if exist('checkInput','var')
+        BuToolsCheckInput = checkInput;
+    else
+        BuToolsCheckInput = true;
+    end
+    
+    if exist('checkPrecision','var')
+        BuToolsCheckPrecision = checkPrecision;
+    else
+        BuToolsCheckPrecision = 1e-12;
+    end
+    
+    fprintf('Global variables: BuToolsVerbose = %d, BuToolsCheckInput = %d, BuToolsCheckPrecision = %g\n',BuToolsVerbose,BuToolsCheckInput,BuToolsCheckPrecision);
+end
+
--- phase.py
+++ phase.py
@@ -1,7 +1,10 @@
-from python.butools.ph import *
+import sys
+import matplotlib as plt
+sys.path.append('butools2/Python')
+from butools.ph import *
 import numpy as np
-a = np.array([[0.1,0.9,0]])
-A = np.array([[-6.2, 2, 0],[2, -9, 1],[1, 0, -3]])
+a = np.matrix([[0.1,0.9,0]])
+A = np.matrix([[-6.2, 2, 0],[2, -9, 1],[1, 0, -3]])
 x = np.arange(0.0,3.002,0.002)
 cdf = CdfFromPH(a, A, x)
-plt.plot(x, cdf)
+print(cdf)
