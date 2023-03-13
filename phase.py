5beb92bf0d93851b757a879fddcdabb0b93a80dd
alirezatko <taaghche.alibakhshi@gmail.com>
2023-03-13 16:05:06 +0330

    butools

--- phase.py
+++ phase.py
@@ -1,22 +1,7 @@
+from butools.ph import *
 import numpy as np
-from pyphs import PhaseSpace
-
-# Define the parameters of the first phase-type distribution
-T1 = np.array([[-1, 0.5], [0.2, -0.3]])
-D1 = np.array([1, 0])
-
-# Define the parameters of the second phase-type distribution
-T2 = np.array([[-0.5, 0.1], [0.3, -0.2]])
-D2 = np.array([0.5, 0.5])
-
-# Define the phase spaces of the two distributions
-ps1 = PhaseSpace(T1, D1)
-ps2 = PhaseSpace(T2, D2)
-
-# Convolve the two phase spaces
-conv_ps = ps1.convolve(ps2)
-
-# Evaluate the PDF of the convolution at a point
-x = 1.5
-pdf_val = conv_ps.pdf(x)
-print("PDF value at x =", x, ":", pdf_val)
+a = np.array([[0.1,0.9,0]])
+A = np.array([[-6.2, 2, 0],[2, -9, 1],[1, 0, -3]])
+x = np.arange(0.0,3.002,0.002)
+cdf = CdfFromPH(a, A, x)
+plt.plot(x, cdf)
