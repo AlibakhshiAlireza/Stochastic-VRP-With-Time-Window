4a157592cb149b3ec98dfb34d26df5a40c3f2b39
Alireza Alibakhshi <alirezaalibakhshi.ie@gmail.com>
2023-03-16 12:16:00 +0330

    modified:   reader.py modified:   utils.py

--- reader.py
+++ reader.py
@@ -3,14 +3,19 @@ import sys
 
 sys.path.append('MPMAVRPMain/Problem')
 sys.path.append('Problem')
-TW = {}
-with open('Problem/A7.txt') as f:
-    contents = f.readlines()
-    nurses = contents[0].split(' ')[0]
-    patients = contents[1].split(' ')[0]
-    TW[0] = [contents[2].split(' ')[0],contents[2].split(' ')[1][:-2]]
-    for index,i in enumerate(contents[3:],start=1):
-        TW[index] = [int(i.split(' ')[0]),int(i.split(' ')[1])]
-   
-print(TW)
+def reader(instance):
+    TW = {}
+    with open('Problem/'+instance+'.txt') as f:
+        contents = f.readlines()
+        nurses = contents[0].split(' ')[0]
+        patients = contents[1].split(' ')[0]
+        TW[0] = [contents[2].split(' ')[0],contents[2].split(' ')[1][:-2]]
+        for index,i in enumerate(contents[3:],start=1):
+            TW[index] = [int(i.split(' ')[0]),int(i.split(' ')[1])]
+    return nurses,patients,TW
+
+nurses,patirnts,TW = reader("A7")
+
+print(patirnts)
+
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from itertools import combinations
-
+from phase import *
+from phasetypes import *
 
 def interleave_zeros(lst):
     new_list = []
@@ -16,11 +17,6 @@ def zeroindexes(x):
 
 
 
-def Zeroidx(Comroute):
-    zero_indexes = [i for i, x in enumerate(Comroute) if x == 0]
-    zero_indexes.pop(0)
-    zero_indexes.pop(-1)
-    return zero_indexes
 
 
 
@@ -40,7 +36,7 @@ def Splitfunc(completeroute,zero_indexes):
             ################ here will be a feasibility check and cost calculation
         steps = steps + 1
 
-def NurseCons(zeroindexes, nurses, testcase):
+def NurseCons(testcase,nurses):
     zero_indexes = zeroindexes(testcase)
     if len(zero_indexes) > nurses:
         a = False
@@ -48,10 +44,26 @@ def NurseCons(zeroindexes, nurses, testcase):
         a = True
     return a
 
-
-
-
-
-
-
-
+TestCase = [0,1,2,0,3,0,4,5,0]
+TW = {0: [0, 360000], 1: [2000, 4000], 2: [3000, 6000], 3: [50000, 70000], 4: [4000, 8000], 5: [1000, 100000]}
+f = zeroindexes(TestCase)
+f.append(len(TestCase))
+probs = []
+start = 0
+for i in f:
+    temp = []
+    p = 0
+    cd = None
+    for j in TestCase[start:i]:
+        if j == 0:
+            cd = TravelTimeDist
+            nxtstp = TestCase[TestCase.index(j) + 1]
+            temp.append(cd.LessThan(TW[nxtstp][1]))
+            state = 1
+        else:
+            cd = phaseconvo(cd,ServiceTimeDist).Dist()
+            cd = phaseconvo(cd,TravelTimeDist).Dist()
+            nxtstp = TestCase[TestCase.index(j) + 1]
+            temp.append(cd.LessThan(TW[nxtstp][1]))
+    start = i
+    print(temp)
