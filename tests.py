da96dbe29a952abded9d7f839809fa619a99c870
Alireza Alibakhshi <alirezaalibakhshi.ie@gmail.com>
2023-03-17 23:25:18 +0330

    Changes to be committed: 	modified:   tests.py

--- tests.py
+++ tests.py
@@ -8,7 +8,8 @@ sys.path.append('butools2/Python')
 sys.path.append('MPMAVRPMain/butools2/Python')
 from butools.ph import *
 from butools.fitting import *
-
+import time
+starttimer = time.time()
 nurses,patients,TW = reader('A7')
 Costmat = matcord(rcord('A7'))
 #OBJ = M + \Sigma cr + \resourcecost
@@ -17,5 +18,5 @@ shuffle(a)
 a = interleave_zeros(a)
 zero_indexes = zeroindexes(a)
 print(Splitfunc(a,zero_indexes,TW,nurses,Costmat))
-
+print(f'For Loop: {time.time() - starttimer} seconds')
 
