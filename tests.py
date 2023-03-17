7ef59a3ebdb464a0837222319d40d71aca13b59f
Alireza Alibakhshi <alirezaalibakhshi.ie@gmail.com>
2023-03-17 12:28:59 +0330

    Changes to be committed: 	new file:   tests.py

---
+++ tests.py
@@ -0,0 +1,11 @@
+from phase import *
+from phasetypes import *
+from reader import *
+import random
+import sys
+sys.path.append('butools2/Python')
+sys.path.append('MPMAVRPMain/butools2/Python')
+from butools.ph import *
+from butools.fitting import *
+
+print(ServiceTimeDist.D)
