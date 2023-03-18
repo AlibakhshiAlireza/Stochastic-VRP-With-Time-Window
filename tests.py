50c704998a1cf445742fa5b1051ed0360f898f48
Alireza <alirezaalibakhshi.ie@gmail.com>
2023-03-18 09:25:10 +0330

    Changes to be committed: 	new file:   Problem/C7.txt 	new file:   Problem/C7C.txt 	modified:   tests.py

--- tests.py
+++ tests.py
@@ -10,13 +10,15 @@ from butools.ph import *
 from butools.fitting import *
 import time
 starttimer = time.time()
-nurses,patients,TW = reader('A7')
-Costmat = matcord(rcord('A7'))
+nurses,patients,TW = reader('C7')
+Costmat = matcord(rcord('C7'))
 #OBJ = M + \Sigma cr + \resourcecost
 a = list(range(1,patients+1))
 shuffle(a)
+print("hi")
 a = interleave_zeros(a)
 zero_indexes = zeroindexes(a)
+print("hi2")
 print(Splitfunc(a,zero_indexes,TW,nurses,Costmat))
 print(f'For Loop: {time.time() - starttimer} seconds')
 
