61aab64933f82c14b78d5969b0390998a8511630
Alireza <alirezaalibakhshi.ie@gmail.com>
2023-04-06 13:21:31 +0430

    modified:   ResultsPrinter.py

--- ResultsPrinter.py
+++ ResultsPrinter.py
@@ -11,37 +11,12 @@ from phasetypes import *
 from butools.ph import *
 from butools.fitting import *
 from matplotlib import rc
-
-with open('Soloutions/C7.txt', 'r') as f:
-    content = f.readlines()
-
-a = content[3].split('[')[-1].split(']')[0].split(',')
-a = [int(i) for i in a]
-b = Split('C7',permutation=a)
-print(len(b[0][0]))
-print(b[2])
-try:
-    chris = np.loadtxt('initsols/C7'+'CHRISTOFIDES.txt',dtype=int,delimiter='-')
-    chris = [int(i) for i in chris]
-except:
-    chris = None
-try:
-    pca = np.loadtxt('initsols/C7'+'PCA.txt',dtype=int,delimiter='-')
-    pca = [int(i) for i in pca]
-except:
-    pca = None
-try:
-    savings = np.loadtxt('initsols/C7' + 'SAVINGS.txt',dtype=int,delimiter='-')
-    savings = [int(i) for i in savings]
-except:
-    savings = None
-lis = [chris,pca,savings]
-for i in lis:
-    if i != None:
-        b = Split('C7',permutation=i)
-        print(len(b[0][0]))
-        print(b[2])
-    else:
-        print('None')
-        print('None')
-
+ins = ['A1','A2','A3','A4','A5','A6','A7','B1','B2','B3','B4','B5','B6','B7','C1','C2','C3','C4','C5','C6','C7']
+for i in ins:
+    with open('PSOSOL/'+i+'.txt', 'r') as f:
+        content = f.readlines()
+        a = content[3].split('[')[-1].split(']')[0].split(',')
+        a = [int(i) for i in a]
+        b = Split(i,permutation=a)
+        print(i,',',len(b[0][0]))
+        print(i,',',b[2])
