37c27b9b506bffdd88c8ccb75df5d58d30247785
Alireza <alirezaalibakhshi.ie@gmail.com>
2023-04-10 22:15:08 +0430

    modified

--- Treader.py
+++ Treader.py
@@ -1,5 +1,6 @@
-import numpy as np
-a = np.loadtxt('Tiguchi\PSOAns.txt', delimiter=',',dtype='float')
-for i in range(16):
-    s = i*5
-    print(a[s][-1],a[s+1][-1],a[s+2][-1],a[s+3][-1],a[s+4][-1])
+ins = ['A1','A2','A3','A4','A5','A6','A7','B1','B2','B3','B4','B5','B6','B7','C1','C2','C3','C4','C5','C6','C7']
+for i in ins:
+    with open('PSOSOL/'+i+'.txt', 'r') as f:
+        a = f.readlines()
+        print(a[0])
+        print(a[1])
